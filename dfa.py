f42fd3acd1c13f4d5e256dab694a5dbd29ac9aa2
p17bail <43962384+p17bail@users.noreply.github.com>
2020-04-06 15:09:36 +0300

    Update dfa.py

--- dfa.py
+++ dfa.py
@@ -8,22 +8,15 @@
 
 
 transitions = { 
-
-	# (Α) Συμπληρώστε τον πίνακα μεταβάσεων ως λεξικό (dictionary).
-	# Η αρχική κατάσταση πρέπει να ονομάζεται 's0'.
-	# Για λεπτομέρειες δείτε στο:
-	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id7
-
+		's0' : {'DIGIT'  : 's1', '.' : 's2'},
+		's1' : {'DIGIT' : 's1', '.' : 's3'},
+		's2' : {'DIGIT' : 's3'},
+                's3' : {'DIGIT' : 's3'}
      	      } 
 
 
 accepts = { 
-
-	# (Β) Συμπληρώστε το λεξικό των καταστάσεων αποδοχής και των
-	# αντίστοιχων επιστρεφόμενων συμβόλων (tokens)
-	# Για λεπτομέρειες δείτε στο:
-	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id8
-
+		"s3" : "FLOAT_TOKEN"
      	  }
 
 
@@ -35,10 +28,8 @@ def get_char(text,pos):
 	
 	c = text[pos]
 	
-	# (Γ) Προαιρετικά, μπορείτε να ομαδοποιήσετε τους
-	# χαρακτήρες εισόδου εδώ.
-	# Για λεπτομέρειες δείτε στο:
-	# http://mixstef.github.io/courses/compilers/lecturedoc/unit1/module1.html#id11
+	if c>= "0" and c <="9":
+		return "DIGIT"
 	
 	return c
 	
